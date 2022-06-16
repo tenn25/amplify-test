350ebf1de2deb21a981769fc9edb5f64feda9cf5
tenn25 <endo_ry@mti.co.jp>
2022-06-17 02:50:59 +0900

    timeline backend & function

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,12 +1,9 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createPost = /* GraphQL */ `
-  mutation CreatePost(
-    $input: CreatePostInput!
-    $condition: ModelPostConditionInput
-  ) {
-    createPost(input: $input, condition: $condition) {
+export const createPostAndTimeline = /* GraphQL */ `
+  mutation CreatePostAndTimeline($content: String!) {
+    createPostAndTimeline(content: $content) {
       type
       id
       content
@@ -49,6 +46,20 @@ export const deleteFollowRelationship = /* GraphQL */ `
     deleteFollowRelationship(input: $input, condition: $condition) {
       followeeId
       followerId
+      timestamp
+    }
+  }
+`;
+export const createPost = /* GraphQL */ `
+  mutation CreatePost(
+    $input: CreatePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    createPost(input: $input, condition: $condition) {
+      type
+      id
+      content
+      owner
       timestamp
     }
   }
